84b34d6e719b2d1528384a8bab24e9a9a147d4d8
ImadDev <minkhantkyaw.99.phoethar@gmail.com>
2022-07-23 15:03:31 +0630

    left to fix wanted.php, featured.php and need to write productdetail.php and checkout.php

--- featured.php
+++ featured.php
@@ -1,4 +1,3 @@
-<!doctype html>
 <html class="no-js" lang="en">
 
     <head>
@@ -18,7 +17,7 @@
 		<link href="https://fonts.googleapis.com/css?family=Lato:100,300,400,700,900" rel="stylesheet">
 		
         <!-- TITLE OF SITE -->
-        <title>Workshop | HGE</title>
+        <title>Featured | HGE</title>
 
         <!-- for title img -->
 		<link rel="shortcut icon" type="image/icon" href="assets/images/logo/favicon.png"/>
@@ -58,15 +57,9 @@
        
 
     </head>
-	
-	<body>
-		<!--[if lte IE 9]>
-            <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="https://browsehappy.com/">upgrade your browser</a> to improve your experience and security.</p>
-        <![endif]-->
+
+    <body>
 		
-		
-		
-		<!--header start-->
 		<section class="header">
 			<div class="container">	
 				<div class="header-left">
@@ -75,14 +68,14 @@
 							<a href="#">
 								<i class="fa fa-phone" aria-hidden="true"></i> +44 000 000 000
 							</a>
-						</li><!--/li-->
+						</li>
 						<li>
 							<a href="#">
 								<i class="fa fa-envelope" aria-hidden="true"></i>mail@hge.com
 							</a>
-						</li><!--/li-->
-					</ul><!--/ul-->
-				</div><!--/.header-left -->
+						</li>
+					</ul>
+				</div>
 				<div class="header-right pull-right">
 					<ul>
 						<li class="reg">
@@ -93,22 +86,20 @@
 							<a href="login.php">
 								Log in
 							</a>
-						</li><!--/li -->
+						</li>
 						
-					</ul><!--/ul -->
-				</div><!--/.header-right -->
-			</div><!--/.container -->	
+					</ul>
+				</div>
+			</div>
 
-		</section><!--/.header-->	
-		<!--header end-->
+		</section>
 		
-		<!--menu start-->
 		<section id="menu">
 			<div class="container">
 				<div class="menubar">
 					<nav class="navbar navbar-default">
 					
-						<!-- Brand and toggle get grouped for better mobile display -->
+						
 						<div class="navbar-header">
 							<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
 								<span class="sr-only">Toggle navigation</span>
@@ -125,13 +116,12 @@
 						<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
 							<ul class="nav navbar-nav navbar-right">
 								<li ><a href="index.html">Home</a></li>
-								<li><a href="information.html">Information</a></li>
+								<li><a href="information.php">Information</a></li>
 								<li><a href="wanted.php">Wanted</a></li>
-								<li class="active"><a href="workshop.html">Workshop</a></li>
+								<li><a href="workshop.html">Workshop</a></li>
 								<li><a href="gallery.php">Gallery</a></li>
 								<li><a href="contact.php">Contact</a></li>
-								<li><a href="featured.php">Featured</a></li>
-								
+								<li class="active"><a href="featured.php">Featured</a></li>
 							</ul><!-- / ul -->
 						</div><!-- /.navbar-collapse -->
 					</nav><!--/nav -->
@@ -140,138 +130,246 @@
 
 		</section><!--/#menu-->
 		<!--menu end-->
-		
-		
-		<section  class="service">
-				<div class="container">
-					<div class="service-details">
-						<div class="section-header text-center">
-							<h2>our services</h2>
-							<p>
-								We provide custom modernised home gym layouts, financial services, warranties and more.
-							</p>
-						</div><!--/.section-header-->
-						<div class="service-content-one">
-							<div class="row">
-								<div class="col-sm-4 col-xs-12">
-									<div class="service-single text-center">
-										<div class="service-img">
-											<img src="assets/images/service/service1.jpg" alt="image of service" />
-										</div><!--/.service-img-->
-										<div class="service-txt">
-											<h2>
-												<a href="#">Layout Planning</a>
-											</h2>
-											<p>
-												Modern Layout Floor Planning for Your Dream Gym 
-											</p>
-											
-										</div><!--/.service-txt-->
-									</div><!--/.service-single-->
-								</div><!--/.col-->
-								<div class="col-sm-4 col-xs-12">
-									<div class="service-single text-center">
-										<div class="service-img">
-											<img src="assets/images/service/service2.png" alt="image of service" />
-										</div><!--/.service-img-->
-										<div class="service-txt">
-											<h2>
-												<a href="#">3 Year Warranty</a>
-											</h2>
-											<p>
-												All new products purchased from our store comes with 3 year extended Warranty 
-											</p>
-											
-										</div><!--/.service-txt-->
-									</div><!--/.service-single-->
-								</div><!--/.col-->
-								<div class="col-sm-4 col-xs-12">
-									<div class="service-single text-center">
-										<div class="service-img">
-											<img src="assets/images/service/service3.jpg" alt="image of service" />
-										</div><!--/.service-img-->
-										<div class="service-txt">
-											<h2>
-												<a href="#">Flexible Payment plans</a>
-											</h2>
-											<p>
-												Associated with Banks and Financial Services for Your Convenience 
-											</p>
-											
-										</div><!--/.service-txt-->
-									</div><!--/.service-single-->
-								</div><!--/.col-->
-							</div><!--/.row-->
-						</div><!--/.service-content-one-->
-					</div><!--/.service-details-->
-				</div><!--/.container-->
-		</section><!--/.service-->
-		<!--service end-->
 
-		<section  class="service-2">
+        <section  class="news">
 			<div class="container">
-				<div class="service-details">
+				<div class="news-details">
 					<div class="section-header text-center">
-						<h2>our repairs</h2>
+						<h2>Featured</h2>
 						<p>
-							With the experienced servicemen, we provide repairs, upgrades and many more.
+							Apple Watch Series 7 is the most popular wearable technology as it works really well within the Apple ecosystem.
 						</p>
 					</div><!--/.section-header-->
-					<div class="service-content-one">
+					<div class="news-card">
 						<div class="row">
-							<div class="col-sm-4 col-xs-12">
-								<div class="service-single text-center">
-									<div class="service-img">
-										<img src="assets/images/service/service4.jpg" alt="image of service" />
-									</div><!--/.service-img-->
-									<div class="service-txt">
-										<h2>
-											<a href="#">Door-to-Door Repair Service</a>
-										</h2>
-										<p>
-											Our service man comes to your home for HGE product repairs.
-										</p>
-										
-									</div><!--/.service-txt-->
-								</div><!--/.service-single-->
+
+							<div class="col-md-8">
+								<div class="blog-left">
+
+									<div class="bl-article-post-img">
+										<a href="blogsinglesideber.html">
+											<img src="assets/images/blog/bl7.webp" alt="image" />
+										</a>
+									</div><!--/.bl-article-post-img-->
+
+									<div class="bl-article-single-txt">
+
+										<div class="bl-single-head">
+											<h3>
+												<a href="#">
+													Tracking fitness journey with Apple Watch Series 7
+												</a>
+											</h3>
+											<p>
+												Posted By :  <span>HGE</span>
+											</p>
+										</div><!--/.bl-single-head-->
+
+										<div class="bl-single-para">
+											<h3>
+												Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accuda doloremque laudantium.
+											</h3>
+											<p>
+												The problem was reported by the BBC back in 2013 and has never gone away. The Construction Index reported that late payments had increased by 27% in 2015, in fact the construction sector has the worst problem for late payment than any other industry sector and accounted for 31% of all payment problems in 2015.
+											</p>
+
+											<blockquote>
+												‘Lorem ipsum dolor sit amet, consectetur pisicing elit,  do he eiusmod tempor incidid ut labore et dolore magna aliqua. Ut enim ad minim veniam jalalaad’  
+											</blockquote>
+											
+											<p>
+												Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+												
+											</p>
+
+											<div class="blog-xtra-body">
+												<div class="row">
+												
+													<div class="col-md-6 col-sm-12 hidden-xs">
+
+														<div class="blog-xtra-img">
+															<div class="news-box-bg blog-body-img">
+																<img src="assets/images/blog/rp1.jpg" alt="blog image">
+															</div><!--/.news-box-bg-->
+															<div class="blog-single-musiq">
+																<a href="https://www.youtube.com/watch?v=lQXzmGg291E" class="vedio-play-icon mfp-iframe">
+																	<img src="assets/images/blog/play-icon.png" alt="play-icon">
+																</a>
+															</div><!--/.blog-single-musiq-->
+														</div><!--/.blog-xtra-img-->
+													</div><!-- /.end of col-md-6 -->
+
+													<div class="col-md-6 col-sm-12">
+															
+														<p class="xtra-para">
+															Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non.Duis aute irure dolor in reprehenderit in volu essa 
+														</p>
+													</div><!--/.col-->
+												</div><!-- /.row-->
+											</div><!--/.blog-xtra-body-->
+											
+											<p>
+												Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+												
+											</p>
+										</div><!--/.bl-single-para-->
+
+										<div class="bl-single-contact-box">
+
+											<ul>
+
+												<li>
+													<div class="bl-list-icon">
+														<ul>
+															
+															<li>
+																<a href="#">
+																	share : 
+																</a>
+															</li><!-- / li -->
+															<li>
+																<a href="#">
+																	<img src="assets/images/blog/facebook.png" alt="facebook">
+																</a>
+															</li><!-- / li -->
+															<li>
+																<a href="#">
+																	<img src="assets/images/blog/g.png" alt="google-plus">
+																</a>
+																
+															</li><!-- / li -->
+															<li>
+																<a href="#">
+																	<img src="assets/images/blog/linkedin.png" alt="linkedin">
+																</a>
+																
+															</li><!-- / li -->
+															<li>
+																<a href="#">
+																	<img src="assets/images/blog/tw.png" alt="twitter">
+																</a>
+															</li><!-- / li -->
+														</ul><!-- / ul -->
+													</div><!-- /.bl-list-icon -->
+												</li>
+
+												<li>
+													<div class="bl-single-tag">
+														<p>
+															Tag : 
+															<span>
+																<a href="#">Business,</a>
+																<a href="#">Property,</a>
+																<a href="#">Finance</a>
+															</span> 
+														</p>
+													</div><!--/.bl-single-tag-->
+												</li>
+
+											</ul><!--/ul-->
+
+										</div><!--/.bl-single-contact-box-->
+
+									</div><!--/.bl-article-single-txt-->
+								</div><!--/.blog-left-->
+
 							</div><!--/.col-->
-							<div class="col-sm-4 col-xs-12">
-								<div class="service-single text-center">
-									<div class="service-img">
-										<img src="assets/images/service/service5.jpg" alt="image of service" />
-									</div><!--/.service-img-->
-									<div class="service-txt">
-										<h2>
-											<a href="#">6 month Limited Warranty </a>
-										</h2>
-										<p>
-											Every repaired products by our service get 6-month limited warranty.
-										</p>
-									</div><!--/.service-txt-->
-								</div><!--/.service-single-->
+
+							<div class="col-md-4">
+								<div class="blog-right">
+
+									<div class="blogs-widget">
+										<h2>Wearable Categories</h2>
+										<div class="blog-categories-list">
+											<ul>
+												<li><a href="#">SmartWatch</a></li>
+												<li><a href="#">Fitness Tracker</a></li>
+												<li><a href="#">Hearables</a></li>
+											</ul>
+										</div><!--/.blog-categories-list-->
+									</div><!--/.blog-widget-->
+
+									<div class="blogs-widget">
+										<h2>Top Featured Products</h2>
+										<div class="blog-related-news">
+											<div class="bl-txt">
+												<div class="bl-img">
+													<a href="#">
+														<img src="assets/images/blog/rp1.jpg" alt="image" />
+													</a>
+												</div><!--/.bl-img-->
+												<p>
+													<a href="#">
+														Apple Watch Series 7
+													</a>
+													<span>Latest Smartwatch from Apple</span>
+												</p>
+											</div><!--/.bl-txt-->
+											<div class="bl-txt">
+												<div class="bl-img">
+													<a href="#">
+														<img src="assets/images/blog/rp2.jpg" alt="image" />
+													</a>
+												</div><!--/.bl-img-->
+												<p>
+													<a href="#">
+														Fitbit Sense
+													</a>
+													<span>Fitness Tracker with smartwatch capabilities</span>
+												</p>
+											</div><!--/.bl-txt-->
+											<div class="bl-txt">
+												<div class="bl-img">
+													<a href="#">
+														<img src="assets/images/blog/rp3.webp" alt="image" />
+													</a>
+												</div><!--/.bl-img-->
+												<p>
+													<a href="#">
+														Hear-hi Acuity Duo TWS Earphones
+													</a>
+													<span>Top Hearable Device</span>
+												</p>
+											</div><!--/.bl-txt-->
+										</div><!--/.blog-related-news-->
+									</div><!--/.blog-widget-->
+
+									<div class="blogs-widget">
+										<h2>Payment Plan</h2>
+										<div class="blog-categories-list">
+											<ul>
+												<li>
+													<a href="#">
+														<i class="fa fa-angle-right" aria-hidden="true"></i>
+														Payment Plan
+													</a>
+												</li>
+												
+											</ul>
+										</div>
+									</div><!--/.blog-widget-->
+
+									
+								</div><!--/.blog-right-->
+
 							</div><!--/.col-->
-							<div class="col-sm-4 col-xs-12">
-								<div class="service-single text-center">
-									<div class="service-img">
-										<img src="assets/images/service/service6.jpg" alt="image of service" />
-									</div><!--/.service-img-->
-									<div class="service-txt">
-										<h2>
-											<a href="#">OEM spare parts</a>
-										</h2>
-										<p>
-											We only use OEM products for your repairs.
-										</p>
-									</div><!--/.service-txt-->
-								</div><!--/.service-single-->
-							</div><!--/.col-->
+
 						</div><!--/.row-->
-					</div><!--/.service-content-one-->
-				</div><!--/.service-details-->
+					</div><!--/.news-card-->
+				</div><!--/news-details-->
 			</div><!--/.container-->
-			
-	</section><!--/.service-->
-	<!--service end-->
+
+		</section><!--/news-->
+		<!--news end-->
+
+        <div class="cookie-disclaimer">
+        <div class="cookie-close accept-cookie"><i class="fa fa-times"></i></div>
+        <div class="container">
+            <p>This is a dummy information for cookie. By using our website, you agree to our <a href="#">Terms & Privacy Policy</a>. 
+            <br>For further information, please contact us via our Contact Us form.</p>
+            <button type="button" class="btn accept-cookie">Agree!</button>
+        </div>
+    </div>
 
 	<!--hm-footer start-->
 	<section class="hm-footer">
@@ -309,13 +407,13 @@
 							</div><!--/.hm-foot-title-->
 							<div class="footer-menu ">	  
 								<ul class="">
-									<li><a href="home.php" >Home</a></li>
+								<li><a href="home.php" >Home</a></li>
 									<li><a href="information.php">Information</a></li>
 									<li><a href="wanted.php">Wanted</a></li>
 									<li><a href="workshop.html">Workshop</a></li>
 									<li><a href="gallery.php">Gallery</a></li>
 									<li><a href="contact.php">Contact us</a></li>
-									<li><a href="featured.php">Featured</a></li> 
+									<li><a href="featured.php">Featured</a></li>
 								</ul>
 							</div><!-- /.footer-menu-->
 						</div><!--/.hm-footer-widget-->
@@ -327,7 +425,7 @@
 							</div><!--/.hm-foot-title-->
 							<div class="hm-para-news">
 								<a href="">
-									You are at the Workshop page.
+									You are at the Featured page.
 								</a>
 								
 							</div><!--/.hm-para-news-->
@@ -377,7 +475,8 @@
 					</div><!--/.col-->
 					
 					<div class="col-sm-5">
-						<div class="foot-menu pull-right">	  
+						<div class="foot-menu pull-right
+						">	  
 							<ul>
 								<li ><a href="#">legal</a></li>
 								<li ><a href="#">sitemap</a></li>
